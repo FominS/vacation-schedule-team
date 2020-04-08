a968c300d937b1c6695a834a2f7de99bbb3fb42c
Sergey <fomin.sn@yandex.ru>
2020-04-08 15:20:26 +0300

    add new component

---
+++ src/components/UnitVacationShedule.vue
@@ -0,0 +1,24 @@
+<template>
+  <GSTC :config="config" @state="onState" />
+</template>
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import GSTC from 'vue-gantt-schedule-timeline-calendar';
+
+class row {
+    
+}
+class Config {
+    height: string;
+
+}
+
+@Component({
+    components: {
+        GSTC
+    }
+})
+export default class UnitVacationShedule extends Vue {
+    private config 
+}
+</script>
